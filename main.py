a0e0682883c6c8e15516aa9fc77280c58e434bd6
sravishta27 <uma.sravista@gmail.com>
2026-02-21 10:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
-
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+from twilio.rest import Client
+api_id=os.environ.get("OWM_API_KEY")
+endpoint="https://api.openweathermap.org/data/2.5/forecast"
+account_sid=os.environ.get("ACCOUNT_SID")
+auth_token=os.environ.get("AUTH_TOKEN")
+weather_params={
+    "lat":17.495922,
+    "lon":78.362968,
+    "appid": api_id,
+    "cnt":4
+}
+response=requests.get(endpoint, params=weather_params)
+response.raise_for_status()
+weather_data=response.json()
+rain=False
+for hour_data in weather_data["list"]:
+    condition_code=(hour_data["weather"][0]["id"])
+    if int(condition_code)<700:
+        rain=True
+if rain:
+    client=Client(account_sid, auth_token)
+    message=client.messages.create(
+        body="It's about to rain,carry an umbrella!",
+        from_='+18572559551',
+        to='+918639017063'
+   )
+    print(message.status)
